7d42b183879181881e7039c35ad5fa2ca5ac821a
Sanaa CHAOU <chaou.sanaa.cs@gmail.com>
2020-07-19 00:49:10 +0100

    fix: default value of role_id to 3 instead of 2

--- database/migrations/2020_07_08_000000_create_users_table.php
+++ database/migrations/2020_07_08_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->unsignedBigInteger('role_id')
-                ->default(2); // default to registered
+                ->default(3); // default to registered
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
