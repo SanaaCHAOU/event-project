16e0db5be6650d96f9c4e0d5fd946391fa36978c
Sanaa CHAOU <chaou.sanaa.cs@gmail.com>
2020-07-19 01:22:52 +0100

    Update the User model to support role based authorization

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password'
     ];
 
     /**
@@ -25,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token'
     ];
 
     /**
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role() {
+        return $this->hasOne('App\Role', 'role_id', 'role_id');
+    }
 }
