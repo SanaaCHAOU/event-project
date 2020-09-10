2a9df6e7bf760998cf07b80ccdc376adbb3913c8
najlae <najlaeelmajdoubi@gmail.com>
2020-09-10 01:59:53 +0100

    add modification

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -1,11 +1,6 @@
 <div class="sidebar" data-color="orange" data-background-color="white" data-image="{{ asset('material') }}/img/sidebar-1.jpg">
-  <!--
-      Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
-
-      Tip 2: you can also add an image using data-image tag
-  -->
   <div class="logo">
-    <a href="https://creative-tim.com/" class="simple-text logo-normal">
+    <a href="#" class="simple-text logo-normal">
       {{ __('BISD-EVENTS') }}
     </a>
   </div>
@@ -60,12 +55,6 @@
         <a class="nav-link" href="{{ route('notifications') }}">
           <i class="material-icons">chrome_reader_mode</i>
           <p>{{ __('Demandes') }}</p>
-        </a>
-      </li>
-      <li class="nav-item{{ $activePage == 'language' ? ' active' : '' }}">
-        <a class="nav-link" href="{{ route('language') }}">
-          <i class="material-icons">language</i>
-          <p>{{ __('RTL Support') }}</p>
         </a>
       </li>
 
